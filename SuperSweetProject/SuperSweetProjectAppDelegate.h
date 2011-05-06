15e16d9a8a7d78ad76c2beff5da160adee090bfb
Kevin Lindeman <klindeman@apple.com>
2011-05-06 10:56:38 -0700

    Initial Commit

---
+++ SuperSweetProject/SuperSweetProjectAppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  SuperSweetProjectAppDelegate.h
+//  SuperSweetProject
+//
+//  Created by Kevin Lindeman on 5/6/11.
+//  Copyright 2011 Apple Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SuperSweetProjectAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+    NSPersistentStoreCoordinator *__persistentStoreCoordinator;
+    NSManagedObjectModel *__managedObjectModel;
+    NSManagedObjectContext *__managedObjectContext;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ SuperSweetProject/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SuperSweetProject
+//
+//  Created by Kevin Lindeman on 5/6/11.
+//  Copyright 2011 Apple Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
